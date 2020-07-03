1cb7af925886402e07fecbe5516cff00b1ac8bcd
Carlos Anibal Lozano Perez <clozano@addi.com>
2020-07-03 12:58:56 -0500

    title added

---
+++ components/title/index.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { Colors } from '../../styles/colors';
+
+export const Title = styled.h3`
+    padding: 8px 4px;
+    color: ${Colors.white};
+    font-family: 'Roboto';
+    font-size: 36px;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.11;
+    letter-spacing: 0.77px;
+    background-color: ${Colors.navyBlue};
+    width: fit-content;
+    white-space: pre-line;
+    margin: 0;
+`; 
